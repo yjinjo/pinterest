6eea1cf0d75ca8ff0f803e602d2b5cfa8ebd965d
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-08 17:22:32 +0900

    #2. DeleteView 기반 회원 탈퇴 구현

--- pragmatic/accountapp/urls.py
+++ pragmatic/accountapp/urls.py
@@ -2,7 +2,12 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from . import views
-from .views import AccountCreateView, AccountDetailView, AccountUpdateView
+from .views import (
+    AccountCreateView,
+    AccountDetailView,
+    AccountUpdateView,
+    AccountDeleteView,
+)
 
 app_name = "accountapp"
 
@@ -17,4 +22,5 @@ urlpatterns = [
     path("create/", AccountCreateView.as_view(), name="create"),
     path("detail/<int:pk>/", AccountDetailView.as_view(), name="detail"),
     path("update/<int:pk>/", AccountUpdateView.as_view(), name="update"),
+    path("delete/<int:pk>/", AccountDeleteView.as_view(), name="delete"),
 ]
