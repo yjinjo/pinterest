be3bd16c89ce887a06ef1ba032c8c91c01104f85
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-08 16:32:18 +0900

    #2. DetailView를 이용한 개인 페이지 구현

--- pragmatic/accountapp/urls.py
+++ pragmatic/accountapp/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from . import views
-from .views import AccountCreateView
+from .views import AccountCreateView, AccountDetailView
 
 app_name = "accountapp"
 
@@ -15,4 +15,5 @@ urlpatterns = [
     ),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("create/", AccountCreateView.as_view(), name="create"),
+    path("detail/<int:pk>/", AccountDetailView.as_view(), name="detail"),
 ]
--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accountapp.models import HelloWorld
 
@@ -33,3 +33,9 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("accountapp:hello_world")
     template_name = "accountapp/create.html"
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = "target_user"
+    template_name = "accountapp/detail.html"
