7872b2845523fa030ac0f680feebe335f9094ff6
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-08 12:24:06 +0900

    #1. DB 정보 접근 및 장고 템플릿 내 for loop

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from accountapp.models import HelloWorld
 
@@ -13,12 +14,12 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
+        return HttpResponseRedirect(reverse("accountapp:hello_world"))
+    else:
+        hello_world_list = HelloWorld.objects.all()
+
         return render(
             request,
             "accountapp/hello_world.html",
-            context={"hello_world_output": new_hello_world},
-        )
-    else:
-        return render(
-            request, "accountapp/hello_world.html", context={"text": "GET METHOD"}
+            context={"hello_world_list": hello_world_list},
         )
