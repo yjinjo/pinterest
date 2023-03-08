aa110f88dd8f41720f55212dabae04d66fbec636
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-08 13:36:39 +0900

    #2. CreateView를 통한 회원가입 구현

--- pragmatic/accountapp/urls.py
+++ pragmatic/accountapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from . import views
+from .views import AccountCreateView
 
 app_name = "accountapp"
 
 urlpatterns = [
     path("hello_world/", views.hello_world, name="hello_world"),
+    path("create/", AccountCreateView.as_view(), name="create"),
 ]
