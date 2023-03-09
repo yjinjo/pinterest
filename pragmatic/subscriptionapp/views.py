8f0df98582dfea43571912988f2a6aea317fbe6d
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-09 20:37:56 +0900

    #9. Field Lookup을 활용한 구독 페이지 구현

--- pragmatic/subscriptionapp/views.py
+++ pragmatic/subscriptionapp/views.py
@@ -2,8 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from projectapp.models import Project
 from subscriptionapp.models import Subscription
 
@@ -27,3 +28,18 @@ class SubscriptionView(RedirectView):
             Subscription(user=user, project=project).save()
 
         return super().get(request, *args, **kwargs)
+
+
+@method_decorator(login_required, "get")
+class SubscriptionListView(ListView):
+    model = Article
+    context_object_name = "article_list"
+    template_name = "subscriptionapp/list.html"
+    paginate_by = 5
+
+    def get_queryset(self):
+        projects = Subscription.objects.filter(user=self.request.user).values_list(
+            "project"
+        )
+        article_list = Article.objects.filter(project__in=projects)
+        return article_list
