736a0de18b902d6509cc043adc0598fb7fd90037
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-09 19:28:20 +0900

    #8. MultipleObjectMixin을 통한 Project와 My Page에 Articles Linking

--- pragmatic/articleapp/models.py
+++ pragmatic/articleapp/models.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from projectapp.models import Project
+
 
 class Article(models.Model):
     writer = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True, related_name="article"
+    )
+    project = models.ForeignKey(
+        Project, on_delete=models.SET_NULL, null=True, related_name="article"
     )
 
     title = models.CharField(max_length=200, null=True)
