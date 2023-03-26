ba9953520847d2f919621d9f68fc5c897db4b311
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-26 09:52:10 +0900

    #11. Update Settings

--- pragmatic/pragmatic/settings/local.py
+++ pragmatic/pragmatic/settings/local.py
@@ -1,19 +1,10 @@
 from .base import *
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("PRAGMATIC_SETTINGS_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     "default": {
