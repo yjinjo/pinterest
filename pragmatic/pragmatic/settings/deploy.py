d3a307cb7d3918f8d57bc05041368c2d194a73d3
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-26 16:58:24 +0900

    #11. Docker secret

--- pragmatic/pragmatic/settings/deploy.py
+++ pragmatic/pragmatic/settings/deploy.py
@@ -4,6 +4,19 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+
+def read_secret(secret_name):
+    file = open("/run/secrets/" + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+
+    return secret
+
+
+SECRET_KEY = read_secret("DJANGO_SECRET_KEY")
+
+
 ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
@@ -11,7 +24,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "django",
         "USER": "django",
-        "PASSWORD": "test1234!",
+        "PASSWORD": read_secret("MYSQL_PASSWORD"),
         "HOST": "mariadb",
         "PORT": "3306",
     }
