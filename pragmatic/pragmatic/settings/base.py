afa5013e39e5f1dc65f352ae51cb2f9b50c7c58c
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-26 11:02:06 +0900

    #11. Update settings

--- pragmatic/pragmatic/settings/base.py
+++ pragmatic/pragmatic/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 from django.urls import reverse_lazy
-import os
+import os, environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -105,13 +105,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
--- pragmatic/pragmatic/settings/deploy.py
+++ pragmatic/pragmatic/settings/deploy.py
@@ -1,11 +1,16 @@
 from .base import *
-from dotenv import load_dotenv
 
-load_dotenv()
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# reading .env file
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("PRAGMATIC_SETTINGS_SECRET_KEY")
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
