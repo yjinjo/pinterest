50cd1f61ece0f927b7b24d92ca2eccb6327a44bb
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-26 10:18:25 +0900

    #11. wsgi error

--- pragmatic/pragmatic/wsgi.py
+++ pragmatic/pragmatic/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pragmatic.settings.deploy")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings/deploy.py")
 
 application = get_wsgi_application()
