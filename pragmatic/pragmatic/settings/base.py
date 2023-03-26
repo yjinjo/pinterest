871f49e1d272b78f52523cf1aceefa9ffe17b90c
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-26 10:53:28 +0900

    #11. Modify settings

--- pragmatic/pragmatic/settings/base.py
+++ pragmatic/pragmatic/settings/base.py
@@ -13,11 +13,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 from django.urls import reverse_lazy
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("PRAGMATIC_SETTINGS_SECRET_KEY")
 
 
 # Application definition
