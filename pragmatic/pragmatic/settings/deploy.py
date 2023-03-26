9fce30ff488bf0b41d429e963376a044699db774
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-26 10:46:49 +0900

    #11. Update settings

--- pragmatic/pragmatic/settings/deploy.py
+++ pragmatic/pragmatic/settings/deploy.py
@@ -1,4 +1,11 @@
 from .base import *
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("PRAGMATIC_SETTINGS_SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
